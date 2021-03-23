1caeee8de32a33861c31e641be6df614e02f5a0e
Benno Lin <blueworrybear@gmail.com>
2021-03-24 03:43:59 +0800

    fix: linting errors

--- addons/addons/LikedPost/src/scripts/entries/forum.tsx
+++ addons/addons/LikedPost/src/scripts/entries/forum.tsx
@@ -3,18 +3,17 @@ import React from "react";
 import { onContent } from "@library/utility/appUtils";
 import { LikeButton } from "../components/LikeButton";
 import { mountReact } from "@vanilla/react-utils";
-import { map } from "lodash";
 
 onContent(() => {
     bootstrap();
 });
 
 function bootstrap() {
-    map(document.getElementsByName("likeButton"), el => {
+    for (const el of document.getElementsByName("likeButton")) {
         const meta = JSON.parse(el.dataset.meta || "");
         mountReact(
             <LikeButton record_type={meta.type} record_id={meta.id} liked={meta.liked} count={meta.count} />,
             el,
         );
-    });
+    }
 }
